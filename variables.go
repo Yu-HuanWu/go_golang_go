29724a335acb8ad586ac271e56ca73d17f94ff40
Yu-HuanWu <yuhuanhwu@gmail.com>
2022-01-26 11:43:16 -0800

    idk

--- variables.go
+++ variables.go
@@ -48,7 +48,7 @@ func main() {
 	var congrats string
 	congrats = "Congratulations"
 	congrats += "!!!"
-	fmt.Println(congrats)
+	fmt.Println(congrats, "!!!")
 	var challenge string = "What else can you do?"
 	fmt.Println(challenge)
 	reminder := "Pratice is important!"
