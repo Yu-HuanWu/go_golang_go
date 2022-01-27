3e1ddf3543bf3b6f47a7929867516aa38b7a2b12
Yu-HuanWu <yuhuanhwu@gmail.com>
2022-01-27 14:41:31 -0800

    func main

--- comicMischief.go
+++ comicMischief.go
@@ -2,3 +2,10 @@ package main
 import {
 	"fmt"
 }
+
+func main(){
+  var publisher, writer, artist, title string
+  var year, pageNumber uint
+  var grade float32
+  fmt.Println(title, "written by", writer, "drawn by", artist)
+}
