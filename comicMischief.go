27b97b2dd5a8219be91b760a57b882b2bf2dee3e
Yu-HuanWu <yuhuanhwu@gmail.com>
2022-01-28 09:27:24 -0800

    variable assigning

--- comicMischief.go
+++ comicMischief.go
@@ -8,4 +8,11 @@ func main(){
   var year, pageNumber uint
   var grade float32
   fmt.Println(title, "written by", writer, "drawn by", artist)
+  title = "Mr. GoToSleep"
+  writer= "Tracey Hatchet"
+  artist= "Jewel Tampson"
+  publisher= "DizzyBooks Publishing Inc."
+  year= 1997
+  pageNumber= 14
+  grade= 6.5
 }
