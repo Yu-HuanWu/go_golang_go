8af30e871a17eeb083f23acd55f127af20c57963
Yu-HuanWu <yuhuanhwu@gmail.com>
2022-01-31 09:00:20 -0800

    Printf

--- fmtPackage.go
+++ fmtPackage.go
@@ -4,8 +4,12 @@ import "fmt"
 
 func main() {
   // There's a mix of Println and Print!
-  fmt.Println("Can", "you", "tell", "the", "difference?")
-  fmt.Print("Between", "these", "two", "methods?") //no space
-  fmt.Print("Anything odd about", "the spacing? \n")
-  fmt.Println("Don't worry if you can't spot it, we'll go through this together!")
+	fmt.Println("Can", "you", "tell", "the", "difference?")
+	fmt.Print("Between", "these", "two", "methods?") //no space
+	fmt.Print("Anything odd about", "the spacing? \n")
+	fmt.Println("Don't worry if you can't spot it, we'll go through this together!")
+
+	animal1 := "cat"
+	animal2 := "dog"
+	fmt.Printf("Are you a %v or a %v person?", animal1, animal2)
 }
