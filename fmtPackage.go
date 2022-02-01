fabe3e568d46c83398b8b9901c11dd307216a0d5
Yu-HuanWu <yuhuanhwu@gmail.com>
2022-02-01 08:34:41 -0800

    Printf with %d and %f and %T

--- fmtPackage.go
+++ fmtPackage.go
@@ -12,4 +12,15 @@ func main() {
 	animal1 := "cat"
 	animal2 := "dog"
 	fmt.Printf("Are you a %v or a %v person?", animal1, animal2)
+	
+	fmt.Println("\n***")
+	floatExample := 1.75
+	fmt.Printf("Working with a %T", floatExample) 
+	fmt.Println("\n***") // Added for spacing
+	yearsOfExp := 3
+	reqYearsExp := 15
+	fmt.Printf("I have %d years of Go experience and this job is asking for %d years.", yearsOfExp, reqYearsExp) 
+	fmt.Println("\n***") // Added for spacing
+	stockPrice := 3.50
+	fmt.Printf("Each share of Gopher feed is $%.2f", stockPrice) 
 }
