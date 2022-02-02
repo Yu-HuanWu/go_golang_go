10c4fa39fb8afd6332611d902809a3c662780d79
Yu-HuanWu <yuhuanhwu@gmail.com>
2022-02-02 08:56:14 -0800

    sprintln

--- fmtPackage.go
+++ fmtPackage.go
@@ -12,7 +12,7 @@ func main() {
 	animal1 := "cat"
 	animal2 := "dog"
 	fmt.Printf("Are you a %v or a %v person?", animal1, animal2)
-	
+
 	fmt.Println("\n***")
 	floatExample := 1.75
 	fmt.Printf("Working with a %T", floatExample) 
@@ -22,5 +22,10 @@ func main() {
 	fmt.Printf("I have %d years of Go experience and this job is asking for %d years.", yearsOfExp, reqYearsExp) 
 	fmt.Println("\n***") // Added for spacing
 	stockPrice := 3.50
-	fmt.Printf("Each share of Gopher feed is $%.2f", stockPrice) 
+	fmt.Printf("Each share of Gopher feed is $%.2f", stockPrice)
+
+	step1 := "Breathe in..."
+	step2 := "Breathe out..."
+	meditation := fmt.Sprintln(step1, step2)
+	fmt.Println(meditation)
 }
