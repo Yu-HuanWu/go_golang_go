160e4073dce23410b3d8cfbfcedeec10a90d08d3
Yu-HuanWu <yuhuanhwu@gmail.com>
2022-02-08 09:19:11 -0800

    >=

--- conditionals.go
+++ conditionals.go
@@ -13,6 +13,11 @@ func main() {
 	lockCombo := "2-35-19"
 	robAttempt := "1-1-1"
 	if (lockCombo == robAttempt) {
-	fmt.Println("The vault is now opened.")
+		fmt.Println("The vault is now opened.")
+	}
+
+	vaultAmt := 2356468
+	if (vaultAmt >= 200000) {
+		fmt.Println("We're going to need more bags.")
 	}
 }
