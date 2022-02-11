0ff0ee1d9fa62a3b62dd9148f81a2f09cdda9ca3
Yu-HuanWu <yuhuanhwu@gmail.com>
2022-02-11 08:35:50 -0800

    else if

--- conditionals.go
+++ conditionals.go
@@ -42,4 +42,13 @@ func main() {
 	} else {
 		fmt.Println("What are we waiting for??")
 	}
+
+	amountStolen := 64650
+	if amountStolen > 1000000 {
+		fmt.Println("We've hit the jackpot!")
+	} else if amountStolen >= 5000 {
+		fmt.Println("Think of all the candy we can buy!")
+	} else {
+		fmt.Println("Why did we even do this?")
+	}
 }
