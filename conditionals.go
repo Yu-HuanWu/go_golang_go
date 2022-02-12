c5031e266b5f0c6d313f82e2b21715fb5afd4c5f
Yu-HuanWu <yuhuanhwu@gmail.com>
2022-02-12 14:34:06 -0800

    switch case

--- conditionals.go
+++ conditionals.go
@@ -51,4 +51,14 @@ func main() {
 	} else {
 		fmt.Println("Why did we even do this?")
 	}
+
+	name := "H. J. Simp"
+	switch name {
+	case "Butch":
+		fmt.Println("Head to Robbers Roost.")
+	case "Bonnie":
+		fmt.Println("Stay put in Joplin.")
+	default:
+		fmt.Println("Just hide!")
+	}
 }
