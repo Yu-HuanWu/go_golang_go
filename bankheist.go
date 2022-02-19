81640922a3d05825e6b36b082e19b9061a97ff38
Yu-HuanWu <yuhuanhwu@gmail.com>
2022-02-19 15:29:33 -0800

    adding conditional

--- bankheist.go
+++ bankheist.go
@@ -7,7 +7,15 @@ import (
 )
 
 func main() {
-  rand.Seed(time.Now().UnixNano())
-  isHeistOn := true
-  eludedGuards := rand.Intn(100)
+	rand.Seed(time.Now().UnixNano())
+  	isHeistOn := true
+  	eludedGuards := rand.Intn(100)
+
+	if eludedGuards >= 50 {
+    	fmt.Println("Looks like you've managed to make it past the guards. Good job, but remember, this is the first step.")
+	} else {
+    	isHeistOn = false
+    	fmt.Println("Plan a better disguise next time?")
+ 	}
+	fmt.Println("is the heist currently on?", isHeistOn)
 }
