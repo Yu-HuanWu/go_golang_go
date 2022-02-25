7ff6bf53d85beb0e52ec432f9566a7a45b6a89e0
Yu-HuanWu <yuhuanhwu@gmail.com>
2022-02-25 08:57:56 -0800

    start game scope

--- function.go
+++ function.go
@@ -6,6 +6,12 @@ func eatTacos() {
   fmt.Println("Yum!")
 }
 
+func startGame() {
+  instructions := "Press enter to start..." 
+  fmt.Println(instructions)
+}
+
 func main() {
   eatTacos()
+  startGame()
 }
