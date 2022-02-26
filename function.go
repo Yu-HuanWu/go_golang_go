e9a492f5c0b4298608b9175c5d60f927ba1fdd9e
Yu-HuanWu <yuhuanhwu@gmail.com>
2022-02-26 09:08:38 -0800

    function return value type

--- function.go
+++ function.go
@@ -1,6 +1,9 @@
 package main
  
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func eatTacos() {
   fmt.Println("Yum!")
@@ -11,7 +14,25 @@ func startGame() {
   fmt.Println(instructions)
 }
 
+func isItLateInNewYork() string {
+  var lateMessage string
+  t := time.Now()
+  tz, _ := time.LoadLocation("America/New_York")
+  nyHour := t.In(tz).Hour()
+  if nyHour < 5 {
+    lateMessage = "Goodness it is late"
+  } else if nyHour < 16 {
+    lateMessage = "It's not late at all!"
+  } else if nyHour < 19 {
+    lateMessage = "I guess it's getting kind of late"
+  } else {
+    lateMessage = "It's late"
+  }
+  return lateMessage
+}
+
 func main() {
   eatTacos()
   startGame()
+  fmt.Println(isItLateInNewYork())
 }
