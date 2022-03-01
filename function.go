07035842b5bb150fa0864425294d0f17e6030a7b
Yu-HuanWu <yuhuanhwu@gmail.com>
2022-03-01 07:22:48 -0800

    multiple return types

--- function.go
+++ function.go
@@ -46,10 +46,39 @@ func combinedSpecialComputation(a, b, c, d float64) (float64, float64, float64, 
 	return specialComputation(a), specialComputation(b), specialComputation(c), specialComputation(d)
 }
 
+func getLikesAndShares(postId int) (int, int){
+  var likesForPost, sharesForPost int
+  switch postId {
+  case 1:
+    likesForPost = 5
+    sharesForPost = 7
+  case 2:
+    likesForPost = 3
+    sharesForPost = 11
+  case 3:
+    likesForPost = 22
+    sharesForPost = 1
+  case 4:
+    likesForPost = 7
+    sharesForPost = 9
+  }
+  fmt.Println("Likes: ", likesForPost, "Shares: ", sharesForPost)
+  return likesForPost, sharesForPost
+}
+
 func main() {
   eatTacos()
   startGame()
   fmt.Println(isItLateInNewYork())
   fmt.Println("On Mars I am", computeMarsYears(33), "years old!")
   fmt.Println(combinedSpecialComputation(.0214, 1.02, 0.312, 4.001))
+	
+  var likes, shares int
+  likes, shares = getLikesAndShares(4)
+  if likes > 5 {
+    fmt.Println("Woohoo! We got some likes.")
+  }
+  if shares > 10 {
+    fmt.Println("We went viral!")
+  }
 }
