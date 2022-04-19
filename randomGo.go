5c6a84c9a0adfc47b6b81c4a0c1b662c4313ae9b
Yu-HuanWu <yuhuanhwu@gmail.com>
2022-04-19 00:18:02 -0700

    swap

--- randomGo.go
+++ randomGo.go
@@ -11,6 +11,10 @@ func add(x, y int) int {
 	return x + y
 }
 
+func swap(x, y string) (string, string) {
+	return y, x
+}
+
 func main() {
 	for i := 0; i < 5; i++ {
 		dur := time.Duration(rand.Intn(1000)) * time.Millisecond
@@ -23,4 +27,7 @@ func main() {
 	fmt.Println("The time is", time.Now())
 	fmt.Println(math.Pi)
 	fmt.Println(add(1337, 42))
+
+	a, b := swap("hello", "world")
+	fmt.Println(a, b)
 }
