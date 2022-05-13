d6b342408982b7ee05564bec5ce68bd37fe0db21
Yu-HuanWu <yuhuanhwu@gmail.com>
2022-05-13 08:18:08 -0700

    time.Now()

--- randomGo.go
+++ randomGo.go
@@ -152,4 +152,14 @@ func main() {
 	default:
 		fmt.Println("Too far away.")
 	}
+
+	t := time.Now()
+	switch {
+	case t.Hour() < 12:
+		fmt.Println("Good morning!")
+	case t.Hour() < 17:
+		fmt.Println("Good afternoon.")
+	default:
+		fmt.Println("Good evening.")
+	}
 }
