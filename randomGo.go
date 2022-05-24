08be051d57a583d5f0e2823ff355af7ef7e14def
Yu-HuanWu <yuhuanhwu@gmail.com>
2022-05-24 09:16:03 -0700

    slice

--- randomGo.go
+++ randomGo.go
@@ -204,6 +204,8 @@ func main() {
 
 	primes := [6]int{2, 3, 5, 7, 11, 13}
 	fmt.Println(primes)
+	var slicedPrimes []int = primes[1:4]
+	fmt.Println(slicedPrimes)
 
 	//.. last
 	fmt.Println("counting")
