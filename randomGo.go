6dab9bd146a1ddf21eb91549115f72237998d9aa
Yu-HuanWu <yuhuanhwu@gmail.com>
2022-06-15 08:39:33 -0700

    toe

--- randomGo.go
+++ randomGo.go
@@ -302,6 +302,7 @@ func main() {
 	board[1][2] = "X"
 	board[1][0] = "O"
 	board[0][2] = "X"
+	board[1][1] = "O"
 
 	for i := 0; i < len(board); i++ {
 		fmt.Printf("%s\n", strings.Join(board[i], " "))
