15955caf0713b5aa38ce7f79b573cd49fdca18ce
Yu-HuanWu <yuhuanhwu@gmail.com>
2022-06-22 08:18:01 -0700

    power

--- randomGo.go
+++ randomGo.go
@@ -357,6 +357,9 @@ func main() {
 	printSlice(appendSlice)
 
 	var pow = []int{1, 2, 4, 8, 16, 32, 64, 128}
+	for index, value := range pow {
+		fmt.Printf("2**%d = %d\n", index, value)
+	}
 
 	//.. last
 	fmt.Println("counting")
